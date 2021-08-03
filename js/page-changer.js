95d04a4daadfb7a0021bfd78199ada62846083d6
daniel-lindem <daniel.lindemann98@gmail.com>
2021-08-03 10:06:18 +0200

    change js code for embed pages

--- js/page-changer.js
+++ js/page-changer.js
@@ -1,53 +1,10 @@
-// preload preload pbie scripts on this page
-var preloadConfig = {
-    type: 'report',
-    baseUrl: 'https://embedded.powerbi.com/reportEmbed',
-};
-  
-var preloadElement = powerbi.preload(preloadConfig);
-  
-preloadElement.onload(function () {
-    console.log("pbie scripts now preloaded into this page");
+var pages = [];
+
+report.getPages().then(function (reportPages) {
+    pages = reportPages;
 });
-  
-  
-// data required for embedding Power BI report
-var embedReportId = "2cf7c5c5-4e1a-4df7-a2a6-21...";
-var embedUrl = "https://app.powerbi.com/reportEmbed?reportId=2cf7c5c5-...";
-var accessToken = "H4sIAAAAAAAEAB1WtQ7sCBL8l5fOSmZaaQMzMzszM49xdf9-o8076K...";
-  
-// Get models object to access enums for embed configuration
-var models = window['powerbi-client'].models;
-  
-var config = {
-    type: 'report',
-    id: embedReportId,
-    embedUrl: embedUrl,
-    accessToken: accessToken,
-    tokenType: models.TokenType.Embed,
-};
-  
-// Get a reference to the embedded report HTML element
-var reportContainer = document.getElementById('embedContainer');
-  
-// call load() instead of embed() to load the report while delaying the rendering process
-var report = powerbi.load(embedContainer, config);
-  
-// when loaded event occurs, set current page then call render()
-report.on("loaded", function () {
-    console.log("loaded event executing");
-    // call to get Pages collection
-    report.getPages().then(
-    function (pages) {
-        // inspect pages in browser console
-        console.log(pages);
-        // display specific page in report
-        var startPage = pages[1]; // this selects the second page
-        config.pageName = startPage.name;
-        // Call report.render() to display report
-        report.render(config);
-    });
-});
+
+pages[1].setActive();
 
 function change_embed_url(display_id){
     embed = "https://app.powerbi.com/reportEmbed?reportId=f4b8cb09-3749-47e4-ad42-9634ce337fdd&autoAuth=true&ctid=7aa99425-1726-430d-bee1-6d96e27ae823&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=";
